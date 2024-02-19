04c97307c170236a8579c566237a4d69b24698d0
vidaa <vidash50@gmail.com>
2024-02-19 17:14:21 -0600

    Adding new functionalities to car task Java Self Practice Task

---
+++ src/day14_practice_tasks_Abs_Poly/carTask/Nio.java
@@ -0,0 +1,27 @@
+package day14_practice_tasks_Abs_Poly.carTask;
+
+public class Nio extends Car implements AutoPilot{
+    public Nio( String model, int year, double price, String color) {
+        super("Nio", model, year, price, color);
+    }
+
+    @Override
+    public void autoPark() {
+
+    }
+
+    @Override
+    public void selfDrive() {
+
+    }
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
---
+++ src/day14_practice_tasks_Abs_Poly/carTask/Tesla.java
@@ -0,0 +1,22 @@
+package day14_practice_tasks_Abs_Poly.carTask;
+
+public class Tesla extends Car implements AutoPark{
+    public Tesla(String model, int year, double price, String color) {
+        super("Tesla", model, year, price, color);
+    }
+
+    @Override
+    public void autoPark() {
+
+    }
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
